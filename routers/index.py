b14d64092e3c02381da08f676ca478b64151fb6c
Magno Belloni <bellonidesouza6@gmail.com>
2025-07-02 21:28:34 -0300

    first commit

---
+++ routers/index.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter
+from fastapi.responses import RedirectResponse
+
+index_router = APIRouter()
+
+@index_router.get("/")
+def index():
+    return RedirectResponse("/docs")
